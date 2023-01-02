b3be0ec19789548da59cb3db017af0c97b428e2b
bagaas86 <bagaas86@gmail.com>
2023-01-02 13:51:25 +0700

    committ

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.template')
+@section('content')
+<div class="page-heading">
+    <h3>Selamat Datang, Admin</h3>
+</div>
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">
+             <h6>Jumlah Pengguna</h6>
+            </div>
+            <div class="card-body">
+            <h2>856</h2>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">
+             <h6>Jumlah Mitra</h6>
+            </div>
+            <div class="card-body">
+                <h2>3342</h2>
+                </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">
+             <h6>Tiket Masif</h6>
+            </div>
+            <div class="card-body">
+                <h2>337</h2>
+                </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="card-header">
+             <h6>Pengunjung</h6>
+            </div>
+            <div class="card-body">
+                <h2>100000</h2>
+                </div>
+        </div>
+    </div>
+
+</div>
+@endsection
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ulinyuk</title>
+    
+    <link rel="stylesheet" href="{{asset('template')}}/dist/assets/css/main/app.css">
+    <link rel="stylesheet" href="{{asset('template')}}/dist/assets/css/main/app-dark.css">
+    <link rel="shortcut icon" href="{{asset('template')}}/dist/assets/images/logo/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('template')}}/dist/assets/images/logo/favicon.png" type="image/png">
+    
+<link rel="stylesheet" href="{{asset('template')}}/dist/assets/css/shared/iconly.css">
+
+<link rel="stylesheet" href="{{asset('template')}}/dist/assets/extensions/simple-datatables/style.css">
+<link rel="stylesheet" href="{{asset('template')}}/dist/assets/css/pages/simple-datatables.css">
+</head>
+
+<body>
+    <div id="app">
+        <div id="sidebar" class="active">
+            @include('layouts/sidebar')     
+        </div>
+
+
+        <div id="main">
+            <header class="mb-3">
+                <a href="{{asset('template')}}/dist/#" class="burger-btn d-block d-xl-none">
+                    <i class="bi bi-justify fs-3"></i>
+                </a>
+            </header>
+            
+{{-- <div class="page-heading">
+    <h3>Selamat Datang, Admin</h3>
+</div> --}}
+<div class="page-content">
+    
+    <section class="row">
+        @yield('content')
+    </section>
+</div>
+
+            {{-- <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="{{asset('template')}}/dist/https://saugi.me">Saugi</a></p>
+                    </div>
+                </div>
+            </footer> --}}
+        </div>
+    </div>
+    <script src="{{asset('template')}}/dist/assets/js/bootstrap.js"></script>
+    <script src="{{asset('template')}}/dist/assets/js/app.js"></script>
+    
+{{-- <!-- Need: Apexcharts -->
+<script src="{{asset('template')}}/dist/assets/extensions/apexcharts/apexcharts.min.js"></script>
+<script src="{{asset('template')}}/dist/assets/js/pages/dashboard.js"></script> --}}
+
+<script src="{{asset('template')}}/dist/assets/extensions/simple-datatables/umd/simple-datatables.js"></script>
+<script src="{{asset('template')}}/dist/assets/js/pages/simple-datatables.js"></script>
+
+</body>
+
+</html>
