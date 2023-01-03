c865e7b4909707784a4e7468fc56015b6823c7fb
gusnanda7 <gus11111234@gmail.com>
2023-01-03 11:02:27 +0700

    coit

--- app/Models/kategori.php
+++ app/Models/kategori.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class kategori extends Model
 {
     use HasFactory;
+
+    public function allData()
+    {
+        return DB::table('kategoris')->get();
+    }
+    public function addData($data)
+    {
+        DB::table('kategoris')->insert($data);
+    }
+    public function deleteData($id_kategori)
+    {
+        DB::table('kategoris')->where('id_kategori', $id_kategori)->delete();
+    }
+    public function editData($id_kategori, $data)
+    {
+        DB::table('kategoris')->where('id_kategori', $id_kategori)->update($data);
+    }
+    public function detailData($id_kategori)
+    {
+        return DB::table('kategoris')->where('id_kategori', $id_kategori)->first();
+    }
 }
