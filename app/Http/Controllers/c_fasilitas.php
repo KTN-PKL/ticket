f4951b2c0bd4d7e87b140c7db6d3a00a5a72a23c
gusnanda7 <gus11111234@gmail.com>
2023-01-03 11:30:54 +0700

    comit

---
+++ app/Http/Controllers/c_fasilitas.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\kategori;
+
+class c_fasilitas extends Controller
+{
+    public function __construct()
+    {
+        $this->fasilitas = new fasilitas();
+    }
+
+    public function index()
+    {
+        $data = ['fasilitas' => $this->fasilitas->allData(),];
+        return view('fasilitas.index', $data);
+    }
+
+    public function create()
+    {
+        return view('fasilitas.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = ['fasilitas' => $request->fasilitas,];
+        $this->fasilitas->addData($data);
+        return redirect()->route('fasilitas')->with('create', 'Mitra Berhasil Dibuat');
+    }
+
+    public function edit($id_fasilitas)
+    {
+        $data = ['fasilitas' => $this->fasilitas->detailData($id_fasilitas),];
+        return view('fasilitas.edit', $data);
+    }
+
+    public function update(Request $request, $id_fasilitas)
+    {
+        $data = ['fasilitas' => $request->fasilitas,];
+        $this->fasilitas->editData($id_fasilitas, $data);
+        return redirect()->route('fasilitas')->with('create', 'Mitra Berhasil Dibuat');
+    }
+
+    public function destroy($id_fasilitas)
+    {
+        $this->fasilitas->deleteData($id_fasilitas);
+        return redirect()->route('fasilitas')->with('create', 'Mitra Berhasil Dibuat');
+    }
+}
--- app/Models/fasilitas.php
+++ app/Models/fasilitas.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class fasilitas extends Model
 {
     use HasFactory;
+
+    public function allData()
+    {
+        return DB::table('fasilitass')->get();
+    }
+    public function addData($data)
+    {
+        DB::table('fasilitass')->insert($data);
+    }
+    public function deleteData($id_fasilitas)
+    {
+        DB::table('fasilitass')->where('id_fasilitas', $id_fasilitas)->delete();
+    }
+    public function editData($id_fasilitas, $data)
+    {
+        DB::table('fasilitass')->where('id_fasilitas', $id_fasilitas)->update($data);
+    }
+    public function detailData($id_fasilitas)
+    {
+        return DB::table('fasilitass')->where('id_fasilitas', $id_fasilitas)->first();
+    }
 }
