652c446a0137221d96c7b2ddba7ac31431f4d7fe
gusnanda7 <gus11111234@gmail.com>
2023-01-03 09:47:48 +0700

    comit

---
+++ database/migrations/2023_01_03_024111_create_pesan_masifs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesan_masifs', function (Blueprint $table) {
+            $table->id('id_masif');
+            $table->string('id_pengguna');
+            $table->string('nik');
+            $table->string('id_paket');
+            $table->string('waktu_kunjungan');
+            $table->string('qty');
+            $table->string('haga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesan_masifs');
+    }
+};
