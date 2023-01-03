0c74d80841e4363cbae0e7985e2159f6b5f1e4d3
gusnanda7 <gus11111234@gmail.com>
2023-01-03 10:45:13 +0700

    comit

---
+++ app/Http/Controllers/c_mitra.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\mitra;
+use Illuminate\Support\Facades\Hash;
+
+class c_mitra extends Controller
+{
+    public function __construct()
+    {
+        $this->mitra = new mitra();
+    }
+
+    public function index()
+    {
+        $data = ['mitra' => $this->mitra->allData(),];
+        return view('mitra.akun.index', $data);
+    }
+
+    public function create()
+    {
+        return view('mitra.akun.create');
+    }
+
+    public function store(Request $request)
+    {
+        $file  = $request->foto;
+        $filename = $request->email.'.'.$file->extension();
+        $file->move(public_path('foto'),$filename);
+        $data = [
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+            'username' => $request->username,
+            'email' => $request->email,
+            'kontak' => $request->kontak,
+            'foto' => $filename,
+            'level' => "mitra",
+            'status' => "active",
+        ];
+        $this->mitra->addDataUser($data);
+        $user = $this->mitra->id($request->email);
+        $data = [
+            'id_mitra' => $user->id,
+            'deskripsi_mitra' => $request->deskripsi_mitra,
+            'balance' => 0,
+        ];
+        $this->mitra->addDataMitra($data);
+        $this->index();
+    }
+
+    public function edit($id_mitra)
+    {
+        $data = ['mitra' => $this->mitra->detailData($id_mitra),];
+        return view('mitra.akun.edit');
+    }
+
+    public function update(Request $request, $id_mitra)
+    {
+        if ($request->foto <> null) {
+            $file  = $request->foto;
+            $filename = $request->email.'.'.$file->extension();
+            $file->move(public_path('foto'),$filename);
+            $data = ['foto' => $filename,];
+            $this->mitra->editUser($id_mitra, $data);
+        }
+        $data = [
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+            'kontak' => $request->kontak,
+        ];
+        $this->mitra->editUser($id_mitra, $data);
+        $data = [
+            'deskripsi_mitra' => $request->deskripsi_mitra,
+        ];
+        $this->mitra->editMitra($id_mitra, $data);
+        $this->index();
+    }
+
+    public function inactive($id_mitra)
+    {
+        $data = ['status' => "inactive"];
+        $this->mitra->editUser($id_mitra, $data);
+        $this->index();
+    }
+
+    public function active($id_mitra)
+    {
+        $data = ['status' => "active"];
+        $this->mitra->editUser($id_mitra, $data);
+        $this->index();
+    }
+
+    public function destroy($id_mitra)
+    {
+        $cek = $this->mitra->detailData($id_mitra);
+        unlink(public_path('foto'). '/' .$cek->foto);
+        $this->mitra->deleteUser($id_mitra);
+        $this->mitra->deleteMitra($id_mitra);
+        $this->index();
+    }
+}
--- app/Models/mitra.php
+++ app/Models/mitra.php
@@ -4,8 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\db;
 
 class mitra extends Model
 {
     use HasFactory;
+
+    public function id($email)
+    {
+        return DB::table('users')->where('email', $email)->first();
+    }
+    public function allData()
+    {
+        return DB::table('users')->join('mitras', 'user.id', '=', 'mitras.id_mitra')->get();
+    }
+    public function addDataUser($data)
+    {
+        DB::table('users')->insert($data);
+    }
+    public function addDataMitra($data)
+    {
+        DB::table('mitras')->insert($data);
+    }
+    public function detailData($id_mitra)
+    {
+        return DB::table('users')->join('mitras', 'user.id', '=', 'mitras.id_mitra')->where('id_mitra', $id_mitra)->first();
+    }
+    public function deleteUser($id)
+    {
+        DB::table('users')->where('id', $id)->delete();
+    }
+    public function deleteMitra($id_mitra)
+    {
+        DB::table('mitras')->where('id_mitra', $id_mitra)->delete();
+    }
+    public function editUser($id, $data)
+    {
+        DB::table('users')->where('id', $id)->update($data);
+    }
+    public function editMitra($id_mitra, $data)
+    {
+        DB::table('mitras')->where('id_mitra', $id_mitra)->update($data);
+    }
 }
