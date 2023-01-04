dfe8f44d9aaa2f1e624df5574556557b23ac6135
gusnanda7 <gus11111234@gmail.com>
2023-01-04 09:29:38 +0700

    comit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\c_mitra;
 use App\Http\Controllers\c_kategori;
 use App\Http\Controllers\c_fasilitas;
+use App\Http\Controllers\c_postingan;
 
 /*
 |--------------------------------------------------------------------------
