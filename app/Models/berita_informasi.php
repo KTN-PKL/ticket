8ebe1f7501577cc056f6e82fa86ac1aa9394de75
gusnanda7 <gus11111234@gmail.com>
2023-01-05 15:43:04 +0700

    comi

--- app/Models/berita_informasi.php
+++ app/Models/berita_informasi.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class berita_informasi extends Model
 {
     use HasFactory;
+    public function allData()
+    {
+        return DB::table('berita_informasis')->get();
+    }
+    public function addData($data)
+    {
+        DB::table('berita_informasis')->insert($data);
+    }
+    public function detailData($id_beritainformasi)
+    {
+        return DB::table('berita_informasis')->where('id_beritainformasi', $id_beritainformasi)->first();
+    }
+    public function editData($id_beritainformasi, $data)
+    {
+        DB::table('berita_informasis')->where('id_beritainformasi', $id_beritainformasi)->edit($data);
+    }
+    public function deleteData($id_beritainformasi)
+    {
+        DB::table('berita_informasis')->where('id_beritainformasi', $id_beritainformasi)->delete();
+    }
 }
