d3a8a99e10fce8cc114a48c03ff700d66749830d
gusnanda7 <gus11111234@gmail.com>
2023-01-07 11:45:31 +0700

    comit

--- resources/views/masif/edit.blade.php
+++ resources/views/masif/edit.blade.php
@@ -40,12 +40,26 @@
                                     </span>
                                   @enderror
                                 </div>
+
                                  <div class="form-group mandatory">
                                     <label for="whatsapp" class="form-label">Whatsapp</label>
                                     <input type="number" class="form-control" placeholder="Masukkan Nomor Whatsapp Penanggung Jawab" value="{{$masif->kontak}}"  data-parsley-required="true" readonly>
                                 </div>
                             </div>
                             <div class="col-md-6 col-12">
+                              <div class="form-group mandatory">
+                                <label for="nik" class="form-label">Waktu_kunjungan</label>
+                                <input type="date" id="waktu" class="form-control"  name="waktu_kunjungan" value="{{ $masif->waktu_kunjungan }}">
+                             @error('waktu_kunjungan')
+                                <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                                </span>
+                              @enderror
+                            </div>
+                            @php
+                                $d = strtotime($masif->waktu_kunjungan);
+                                $N = date('l' ,$d);
+                            @endphp
                               <div class="form-group mandatory">
                                 <label for="kategori">Kategori</label>
                                 <select name="kategori" type="text" class="form-select" onchange="readwisata({{ $masif->id_masif }})" id="kategori">
@@ -65,7 +79,16 @@
                             </div>
                         <div class="form-group mandatory">
                           <label for="username" class="form-label">Harga</label>
-                          <input type="number" id="username" class="form-control" placeholder="Masukkan Harga Satuan" name="username" value="{{ $masif->harga_wday }}"  data-parsley-required="true">
+                          <input type="number" id="username" class="form-control" placeholder="Masukkan Harga Satuan" name="username" @if ($masif->harga == null)
+                              @if ($N == "Saturday" || $N == "Sunday")
+                              value="{{ $masif->harga_wend }}"
+                              @else
+                              value="{{ $masif->harga_wday }}"
+                              @endif
+                          @else
+                          value="{{ $masif->harga }}"
+                          @endif
+                           data-parsley-required="true">
                       </div>
                             </div>
                         </div>
@@ -97,7 +120,7 @@
           $("#wisata").html(data);
       });
     }
-    
+  
 </script>
 
 @endsection
